f41f45269e92ded1280538ac2c5859b262e49fb5
Ivan Mitrichev <imitrichev@users.noreply.github.com>
2017-10-04 04:28:49 +0300

    Add files via upload

---
+++ l04_16.cpp
@@ -0,0 +1,20 @@
+#include <iostream>       // std::cout
+#include <omp.h>
+using namespace std;
+
+int main()
+{
+	int tid=777;
+	omp_set_num_threads(2);
+
+	#pragma omp parallel num_threads(3) firstprivate (n)
+	{
+		tid=omp_get_thread_num()+8;
+		cout <<tid<<endl;
+		#pragma omp single nowait
+		// prints 8 from master thread 0
+			cout<<"single:" << tid << endl;
+	}
+
+	return 0;
+}
