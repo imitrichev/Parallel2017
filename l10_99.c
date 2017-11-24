bcd21772b625c89d13b380e96c5da2662ed83281
Ivan Mitrichev <imitrichev@users.noreply.github.com>
2017-11-24 04:25:45 +0300

    Create l10_99.c
    mpi+openmp example

---
+++ l10_99.c
@@ -0,0 +1,27 @@
+// mpicc -fopenmp l10_99.c -lgomp -o l10
+// export OMP_NUM_THREADS=4
+// mpirun ./l10
+#include <stdio.h>
+#include "mpi.h"
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+  int numprocs, rank, namelen;
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int iam = 0, np = 1;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Get_processor_name(processor_name, &namelen);
+
+  #pragma omp parallel default(shared) private(iam, np)
+  {
+    np = omp_get_num_threads();
+    iam = omp_get_thread_num();
+    printf("Hello from thread %d out of %d from process %d out of %d on %s\n",
+           iam, np, rank, numprocs, processor_name);
+  }
+
+  MPI_Finalize();
+}
